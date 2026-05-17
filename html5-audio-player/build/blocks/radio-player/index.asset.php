f55cb3803b5affb9cf7fcf3507aa19d11f3a9b5d
Sean Murphy <sean@iamseanmurphy.com>
2026-05-17 18:34:21 +0000

    2.6.2

--- html5-audio-player/build/blocks/radio-player/index.asset.php
+++ html5-audio-player/build/blocks/radio-player/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '52c6359601a2cce1e226');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1a8a57ae7c47189685b6');
--- html5-audio-player/build/dashboard.asset.php
+++ html5-audio-player/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'dcd1f80dc034650348e5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '67880fdd8169bea49872');
